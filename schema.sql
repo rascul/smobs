6377b92a32ebec6aa6c3405b82569526017a1e5f
rascul <rascul3@gmail.com>
2013-10-14 16:51:08 -0400

    update schema, possibly finalized

--- schema.sql
+++ schema.sql
@@ -1,21 +1,25 @@
 CREATE TABLE smob (
   smobid INTEGER PRIMARY KEY,
-  name TEXT UNIQUE
+  name TEXT UNIQUE,
+  stab INTEGER,
+  channel INTEGER
 );
-
 CREATE TABLE load (
+  id INTEGER PRIMARY KEY,
   smobid INTEGER,
-  item TEXT,
-  count INTEGER,
+  who TEXT,
   date TEXT,
   FOREIGN KEY(smobid) REFERENCES smob(id)
 );
-
 CREATE TABLE item (
   id INTEGER PRIMARY KEY,
-  name TEXT UNIQUE,
-  type TEXT,
-  found_in TEXT
+  name TEXT
 );
-
-CREATE TABLE 
+CREATE TABLE load_item (
+  loadid INTEGER,
+  itemid INTEGER,
+  quantity INTEGER,
+  location TEXT,
+  FOREIGN KEY(loadid) REFERENCES load(id),
+  FOREIGN KEY(itemid) REFERENCES item(id)
+);
