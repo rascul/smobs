43f7a2ad2ef69393df16499859810447b6f93dad
rascul <rascul3@gmail.com>
2013-10-18 12:07:25 -0400

    do what the fuck you want public license

--- smobs.py
+++ smobs.py
@@ -1,3 +1,8 @@
+# Copyright (C) 2013 Ray Schulz <rascul3@gmail.com>
+# This work is free. You can redistribute it and/or modify it under the
+# terms of the Do What The Fuck You Want To Public License, Version 2,
+# as published by Sam Hocevar. See the COPYING file for more details.
+
 import re
 import psycopg2
 from flask import Flask, render_template, request, g
